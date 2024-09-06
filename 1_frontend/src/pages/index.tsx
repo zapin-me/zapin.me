97fe86e29f79ec850a474f5687e6cfbdb6a8780b
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-09-05 23:16:56 -0300

    fix: pop up break-words (#3)

--- 1_frontend/src/pages/index.tsx
+++ 1_frontend/src/pages/index.tsx
@@ -150,7 +150,7 @@ export default function Home() {
       setMarkerList((prevInvoices) => [messageParsed, ...prevInvoices]);
       setRunConfetti(true);
       fetchTotalPins();
-      setActiveMarkerId(messageParsed.id);
+      // setActiveMarkerId(messageParsed.id);
 
       setTimeout(() => {
         setRunConfetti(false);
